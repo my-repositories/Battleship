aa1b7feb3f3386d9135beebcd6ae562fcfee69c3
loktionov129 <loktionov129@gmail.com>
2017-12-15 23:00:56 +0700

    [logs]: Исправлены пути

--- cmd/Logger.cs
+++ cmd/Logger.cs
@@ -1,24 +1,24 @@
 ﻿using System;
-using System.IO;
-using System.Diagnostics;
 
 // TODO: REMOVE IT AFTER RELEASE v.1
 namespace cmd
 {
     public static class Logger
     {
-        private static readonly string _logsDirectory = "./logs";
-        private static readonly string _logName;
+#if !DEBUG
+        private static readonly string LogsDirectory = $".{System.IO.Path.DirectorySeparatorChar}logs";
+        private static readonly string LogName;
 
         static Logger()
         {
-            _logName = $"{_logsDirectory}/{GetFormattedDate()}.txt";
-            Directory.CreateDirectory(_logsDirectory);
+            LogName = $"{LogsDirectory}{System.IO.Path.DirectorySeparatorChar}{GetFormattedDate()}.txt";
+            System.IO.Directory.CreateDirectory(LogsDirectory);
         }
+#endif
 
         public static void Write(Challenger challenger, string message)
         {
-            Write(challenger.GetType().Name + message);
+            Write(challenger.GetType().Name + "." + message);
         }
 
         public static void Write(string message)
@@ -26,15 +26,15 @@ namespace cmd
             var dateTime = GetFormattedDate();
             var log = $"[{dateTime}] {message}";
 #if DEBUG
-            Debug.WriteLine(log);
+            System.Diagnostics.Debug.WriteLine(log);
 #else
-            File.AppendAllText(_logName, log + Environment.NewLine);
+            System.IO.File.AppendAllText(LogName, log + Environment.NewLine);
 #endif
         }
 
         private static string GetFormattedDate()
         {
-            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            return DateTime.Now.ToString("yyyy.MM.dd HH-mm-ss");
         }
     }
 }
